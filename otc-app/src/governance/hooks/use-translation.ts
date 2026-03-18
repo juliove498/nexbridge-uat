0c2f22f62b905caf8b589e6bd17112ea51862826
juliove498 <julio.ventura1980@gmail.com>
2026-03-18 16:35:57 -0300

    removing otc-app from landing

--- otc-app/src/governance/hooks/use-translation.ts
+++
@@ -1,8 +0,0 @@
-import { useLanguageStore } from "@/governance/store/language-store";
-import { getTranslations } from "@/governance/i18n";
-import type { Translations, Locale } from "@/governance/i18n/types";
-
-export function useTranslation(): { t: Translations; locale: Locale } {
-  const locale = useLanguageStore((s) => s.locale);
-  return { t: getTranslations(locale), locale };
-}
